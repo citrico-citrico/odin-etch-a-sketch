9606440256b4bc861cdf5e1c042ea0ebf018f3d8
citrico-citrico <samytorrez4@gmail.com>
2026-05-21 15:52:03 -0500

    Changed the name of the div elements.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 container = document.querySelector("#container");
 
 for (let i = 0; i < 16 * 16; i++){
-const newDiv = document.createElement("div");
-newDiv.className = "list-item";
-container.appendChild(newDiv);
+const divs = document.createElement("div");
+divs.className = "list-item";
+container.appendChild(divs);
 }
+
