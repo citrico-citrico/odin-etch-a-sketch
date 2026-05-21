f3a3b6f6889763bb000155903a2dbb05da69e8f3
citrico-citrico <samytorrez4@gmail.com>
2026-05-21 16:06:48 -0500

    Added an eventlistener to the divs.
    The eventlistener is included inside the loop function that creates the divs.
    Once the mouse button hovers over the divs the background color is changed to gray.

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ for (let i = 0; i < 16 * 16; i++){
 const divs = document.createElement("div");
 divs.className = "list-item";
 container.appendChild(divs);
+
+divs.addEventListener('mouseenter', () => {
+    divs.style.backgroundColor = 'gray';
+});
 }
 
